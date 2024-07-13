6185aaba67845a0fc32466d2aa6afca926ec2332
Liam, Song <blackdogcode@gmail.com>
2024-07-13 10:03:10 +0900

    002 Python Course Project

---
+++ 002_TipCalculator/main.py
@@ -0,0 +1,16 @@
+print("Welcome to the tip calculator!")
+
+print("What was the bill?")
+bill = float(input("$"))
+
+print("What percentage tip would you like to give? e.g 10%, 15%, 20%")
+tip_percent = float(input()[:-1])
+
+print("How many people to split the bill?")
+num_people = int(input())
+
+total_bill = bill * (1 + tip_percent / 100)
+each_person_pay = round(total_bill / num_people, 2)
+each_person_pay = "{:.2f}".format(each_person_pay)
+
+print(f"Each person should pay: ${each_person_pay}")
